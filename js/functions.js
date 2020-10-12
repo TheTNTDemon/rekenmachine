350c13e08eee8765f417c9e49097588c5e6612d4
raunow <26925392+Raunow@users.noreply.github.com>
2020-10-12 20:52:51 +0200

    Refactored Calculator

--- js/functions.js
+++ js/functions.js
@@ -1,129 +1,52 @@
-// bool for answered state
-let answered = false;
 
-// trigger when number button is pressed
-function BtnPressed(PressedNumber) {
-    // console.log("clicked number " + PressedNumber);
-
-    if (answered) {
-        answered = false;
-        Clear();
-    }
-
-    document.getElementById("scherm").innerHTML += PressedNumber;
-}
-
-// trigger when utility button is pressed
-function UBtnPressed(PressedUtil) {
-    // console.log("clicked util " + PressedUtil);
-    if (ContainsUtility(PressedUtil)) {
+function utilBtnPressed(modifier) {
+    if (keyMap[modifier] && getElement('display').innerHTML.endsWith(modifier))
         return;
-    }
 
-    document.getElementById("scherm").innerHTML += PressedUtil;
+    addToDisplay(modifier);
 }
 
-// check if last character was a utility returns true/false
-function ContainsUtility(PressedUtil) {
-    let screen = document.getElementById("scherm");
+const Clear = () => getElement('display').innerHTML = ' ';
 
-    if (PressedUtil == ".") {
-        if (screen.innerHTML.includes(".")) {
-            return true;
-        }
-    }
-
-    if (screen.innerHTML.endsWith("+") || 
-        screen.innerHTML.endsWith("*") || 
-        screen.innerHTML.endsWith("/") || 
-        screen.innerHTML.endsWith("-")) {
-        return true;
-    }
-
-    return false;
-}
-
-// clear calculator screen
-function Clear() {
-    // console.log("cleared the screen");
-    document.getElementById("scherm").innerHTML = "";
-}
-
-// display answer and add to history
-function Answer() {
-    let screen = document.getElementById("scherm");
-    let history = document.getElementById("history");
-    var answer = eval(screen.innerText);
-    // console.log("answer is: " + answer);
-    answered = true;
-    history.innerHTML += screen.innerHTML + "=" + answer + "<br>"
-    screen.innerHTML = answer;
-}
-
-// remove last character 
-function Backspace() {
-    let screen = document.getElementById("scherm");
+const Backspace = () => {
+    let screen = getElement('display');
     screen.innerHTML = screen.innerHTML.slice(0, -1);
 }
 
-// listen to keydown event
-document.addEventListener("keydown", onKeyDown, true);
+function Answer() {
+    let screen = getElement('display');
+    let history = getElement('history');
+    answer = math.evaluate(screen.innerText);
 
-// see which key is pressed
-function onKeyDown(e) {
-    switch (e.which) {
-        case 8:
-            Backspace();
-            break;
-        case 48:
-            BtnPressed(0);
-            break;
-        case 49:
-            BtnPressed(1);
-            break;
-        case 50:
-            BtnPressed(2);
-            break;
-        case 51:
-            BtnPressed(3);
-            break;
-        case 52:
-            BtnPressed(4);
-            break;
-        case 53:
-            BtnPressed(5);
-            break;
-        case 54:
-            BtnPressed(6);
-            break;
-        case 55:
-            BtnPressed(7);
-            break;
-        case 56:
-            BtnPressed(8);
-            break;
-        case 57:
-            BtnPressed(9);
-            break;
-        case 67:
-            Clear();
-            break;
-        case 106:
-            UBtnPressed("*");
-            break;
-        case 107:
-            UBtnPressed("+");
-            break;
-        case 109:
-            UBtnPressed("-");
-            break;
-        case 111:
-            UBtnPressed("/");
-            break;
-        case 187:
-            Answer();
-            break;
-        case 190:
-            UBtnPressed(".");
-    }
+    history.innerHTML += screen.innerHTML + ' = ' + answer + '<br>'
+    screen.innerHTML = answer;
 }
+
+function onInput(value) {
+    if (answer) {
+        answer = null;
+        Clear();
+    }
+
+    if (value >= 0 && value <= 9) return addToDisplay(value);
+    if (keyMap[value]) keyMap[value](value);
+}
+
+var keyMap = {
+    '=': Answer,
+    'c': Clear,
+    'Backspace': Backspace,
+    '+': utilBtnPressed,
+    '-': utilBtnPressed,
+    '*': utilBtnPressed,
+    '/': utilBtnPressed,
+    '.': utilBtnPressed,
+
+}
+
+var answer = null;
+
+const getElement = (id) => document.getElementById(id);
+const addToDisplay = (value) => getElement('display').innerHTML += value;
+
+document.addEventListener('keydown', (e) => onInput(e.key), true);
